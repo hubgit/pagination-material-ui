1de5a716473e31a37a56370c11754720fc439aaa
endigo <endigo.18@gmail.com>
2016-11-23 15:53:59 +0800

    minified

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var isDev = (process.env.NODE_ENV === 'development');
+
+var defineEnvPlugin = new webpack.DefinePlugin({
+  __DEV__: isDev
+});
+
+var entryScripts = ['./src.js'];
+var output = {
+  path: __dirname,
+  filename: 'index.js'
+};
+
+var plugins = [
+  defineEnvPlugin,
+  new webpack.NoErrorsPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
+  })
+];
+
+var moduleLoaders = [
+  {
+    test: /\.js$/,
+    loaders: [ 'babel' ],
+    exclude: /node_modules/,
+    include: __dirname
+  }
+];
+
+module.exports = {
+  devtool: isDev ? 'eval': '',
+  entry: entryScripts,
+  output: output,
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: moduleLoaders
+  }
+};
